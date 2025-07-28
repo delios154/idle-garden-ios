3d1cc3f6c2b7a0e70c6238bf9da0c8df2d311a58
delios154 <almansoori.154@gmail.com>
2025-07-28 19:47:45 +0400

    Initial Commit

---
+++ Idle GardenTests/Idle_GardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  Idle_GardenTests.swift
+//  Idle GardenTests
+//
+//  Created by Mohammed Almansoori on 28/07/2025.
+//
+
+import Testing
+@testable import Idle_Garden
+
+struct Idle_GardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
